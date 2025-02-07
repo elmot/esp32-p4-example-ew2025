566c686ef4240c5641ec143d00d25f127b147c92
Ilia (Elias) Motornyi <elijah.mot@gmail.com>
2025-02-07 14:15:02 +0200

    Use checker pattern if camera is off

--- main/camera.c
+++ main/camera.c
@@ -15,7 +15,9 @@
 #include "usb/uvc_host.h"
 
 #include "camera.h"
-//#define FRAME_FORMAT UVC_VS_FORMAT_MJPEG
+
+#include <string.h>
+
 #define FRAME_FORMAT UVC_VS_FORMAT_YUY2
 #define FRAME_FPS    30
 #define DECODE_EVERY_XTH_FRAME 2        // Every 2nd frame will be decoded and send to display. This save CPU time
@@ -27,55 +29,63 @@
 #define NUMBER_OF_FRAME_BUFFERS 2 // Number of frames from the camera
 #endif
 
-uint16_t *cam_buffer = NULL; // Framebuffer for decoded data (to LCD)
-static const char *TAG = "UVC_CAM";
+uint16_t* cam_buffer = NULL; // Framebuffer for decoded data (to LCD)
+static const char* TAG = "UVC_CAM";
 static QueueHandle_t frame_q = NULL; // Queue of received frames that are passed to processing task
 static SemaphoreHandle_t device_disconnected_sem;
 static uvc_host_stream_hdl_t stream;
 
-void yuy2_to_rgb565(const uint8_t *yuy2, uint16_t *rgb565, unsigned int width,unsigned  int height);
+void yuy2_to_rgb565(const uint8_t* yuy2, uint16_t* rgb565, unsigned int width, unsigned int height);
 
-void stream_callback(const uvc_host_stream_event_data_t *event, void *user_ctx)
+// ReSharper disable once CppParameterMayBeConstPtrOrRef
+void stream_callback(const uvc_host_stream_event_data_t* event,void* user_ctx)
 {
-    (void) user_ctx;
-    switch (event->type) {
-        case UVC_HOST_TRANSFER_ERROR:
-            ESP_LOGE(TAG, "USB error");
-            break;
-        case UVC_HOST_DEVICE_DISCONNECTED:
-            ESP_LOGW(TAG, "Device disconnected");
-            ESP_ERROR_CHECK(uvc_host_stream_close(event->device_disconnected.stream_hdl));
-            xSemaphoreGive(device_disconnected_sem);
-            break;
-        case UVC_HOST_FRAME_BUFFER_OVERFLOW:
-            ESP_LOGW(TAG, "Frame buffer overflow");
-            break;
-        case UVC_HOST_FRAME_BUFFER_UNDERFLOW:
-            ESP_LOGW(TAG, "Frame buffer underflow");
-            break;
-        default:
-            abort();
+    (void)user_ctx;
+    switch (event->type)
+    {
+    case UVC_HOST_TRANSFER_ERROR:
+        ESP_LOGE(TAG, "USB error");
+        break;
+    case UVC_HOST_DEVICE_DISCONNECTED:
+        ESP_LOGW(TAG, "Device disconnected");
+        ESP_ERROR_CHECK(uvc_host_stream_close(event->device_disconnected.stream_hdl));
+        xSemaphoreGive(device_disconnected_sem);
+        break;
+    case UVC_HOST_FRAME_BUFFER_OVERFLOW:
+        ESP_LOGW(TAG, "Frame buffer overflow");
+        break;
+    case UVC_HOST_FRAME_BUFFER_UNDERFLOW:
+        ESP_LOGW(TAG, "Frame buffer underflow");
+        break;
+    default:
+        abort();
     }
 }
 
+// ReSharper disable once CppParameterMayBeConstPtrOrRef
 bool frame_callback(const uvc_host_frame_t *frame, void *user_ctx)
 {
-    (void) user_ctx;
-    bool frame_processed = false; // If we return false from this callback, we must return the frame with uvc_host_frame_return(stream, frame);
+    (void)user_ctx;
+    bool frame_processed = false;
+    // If we return false from this callback, we must return the frame with uvc_host_frame_return(stream, frame);
 
-    switch (frame->vs_format.format) {
-        case UVC_VS_FORMAT_YUY2: {
+    switch (frame->vs_format.format)
+    {
+    case UVC_VS_FORMAT_YUY2:
+        {
             ESP_LOGD(TAG, "YUY2 frame %dx%d", frame->vs_format.h_res, frame->vs_format.v_res);
-            if (cam_buffer) {
+            if (cam_buffer)
+            {
                 yuy2_to_rgb565(frame->data, (uint16_t*)cam_buffer, frame->vs_format.h_res, frame->vs_format.v_res);
-//                esp_lcd_panel_draw_bitmap(panel_handle, 0, 0, (int)frame->vs_format.h_res, (int)frame->vs_format.v_res, (const void *)fb);
+                //                esp_lcd_panel_draw_bitmap(panel_handle, 0, 0, (int)frame->vs_format.h_res, (int)frame->vs_format.v_res, (const void *)fb);
             }
             frame_processed = true;
             break;
         }
-        case UVC_VS_FORMAT_H264:
-        case UVC_VS_FORMAT_H265:
-        case UVC_VS_FORMAT_MJPEG: {
+    case UVC_VS_FORMAT_H264:
+    case UVC_VS_FORMAT_H265:
+    case UVC_VS_FORMAT_MJPEG:
+        {
             // Attempt to put the new frame into the queue without checking if it is full.
             // If successful, we save processing time since we avoid the overhead of a full check for every frame.
             BaseType_t frame_put_to_queue = xQueueSendToBack(frame_q, &frame, 0);
@@ -84,50 +94,58 @@ bool frame_callback(const uvc_host_frame_t *frame, void *user_ctx)
             assert(frame_put_to_queue == pdPASS);
             break;
         }
-        default:
-            ESP_LOGI(TAG, "Unsupported format!");
-            break;
+    default:
+        ESP_LOGI(TAG, "Unsupported format!");
+        break;
     }
     return frame_processed;
 }
 
+// ReSharper disable once CppParameterMayBeConstPtrOrRef
 _Noreturn static void processing_task(void *pvParameters)
 {
-    (void) pvParameters;
-    uvc_host_frame_t *frame;
-    uint8_t *jpeg_working_buffer = malloc(DECODE_WORKING_BUFFER_SIZE);
+    (void)pvParameters;
+    uvc_host_frame_t* frame;
+    uint8_t* jpeg_working_buffer = malloc(DECODE_WORKING_BUFFER_SIZE);
     assert(jpeg_working_buffer);
 
-    while (1) {
+    while (1)
+    {
         xQueueReceive(frame_q, &frame, portMAX_DELAY);
         ESP_LOGD(TAG, "MJPEG frame %dx%d %d bytes", frame->vs_format.h_res, frame->vs_format.v_res, frame->data_len);
 
         static int frame_i = 0;
-        if (cam_buffer && ((frame_i % DECODE_EVERY_XTH_FRAME) == 0)) {
+        if (cam_buffer && ((frame_i % DECODE_EVERY_XTH_FRAME) == 0))
+        {
             frame_i = 0;
             esp_jpeg_image_cfg_t jpeg_cfg = {
-                    .indata = (uint8_t *)frame->data,
-                    .indata_size = frame->data_len,
-                    .outbuf = (uint8_t *)cam_buffer,
-                    .outbuf_size = FRAME_H_RES * FRAME_V_RES * 2,
-                    .out_format = JPEG_IMAGE_FORMAT_RGB565,
-                    .out_scale = JPEG_IMAGE_SCALE_0,
-                    .flags = {
-                            .swap_color_bytes = 0,
-                    },
-                    .advanced = {
-                            .working_buffer = jpeg_working_buffer,
-                            .working_buffer_size = DECODE_WORKING_BUFFER_SIZE,
-                    },
+                .indata = (uint8_t*)frame->data,
+                .indata_size = frame->data_len,
+                .outbuf = (uint8_t*)cam_buffer,
+                .outbuf_size = FRAME_H_RES * FRAME_V_RES * 2,
+                .out_format = JPEG_IMAGE_FORMAT_RGB565,
+                .out_scale = JPEG_IMAGE_SCALE_0,
+                .flags = {
+                    .swap_color_bytes = 0,
+                },
+                .advanced = {
+                    .working_buffer = jpeg_working_buffer,
+                    .working_buffer_size = DECODE_WORKING_BUFFER_SIZE,
+                },
             };
             esp_jpeg_image_output_t outimg;
 
-            if (ESP_OK == esp_jpeg_decode(&jpeg_cfg, &outimg)) {
-//                esp_lcd_panel_draw_bitmap(panel_handle, 0, 0, outimg.width, outimg.height, (const void *)fb);
-            } else {
+            if (ESP_OK == esp_jpeg_decode(&jpeg_cfg, &outimg))
+            {
+                //                esp_lcd_panel_draw_bitmap(panel_handle, 0, 0, outimg.width, outimg.height, (const void *)fb);
+            }
+            else
+            {
                 ESP_LOGW(TAG, "Decoding failed");
             }
-        } else {
+        }
+        else
+        {
             ESP_LOGI(TAG, "Skipping decoding of received MJPEG frame");
         }
         frame_i++;
@@ -140,29 +158,57 @@ _Noreturn static void processing_task(void *pvParameters)
     //vTaskDelete(NULL);
 }
 
+// ReSharper disable once CppParameterMayBeConstPtrOrRef
 _Noreturn static void usb_lib_task(void *arg)
 {
-    (void) arg;
-    while (1) {
+    (void)arg;
+    while (1)
+    {
         // Start handling system events
         uint32_t event_flags;
         usb_host_lib_handle_events(portMAX_DELAY, &event_flags);
-        if (event_flags & USB_HOST_LIB_EVENT_FLAGS_NO_CLIENTS) {
+        if (event_flags & USB_HOST_LIB_EVENT_FLAGS_NO_CLIENTS)
+        {
             usb_host_device_free_all();
         }
-        if (event_flags & USB_HOST_LIB_EVENT_FLAGS_ALL_FREE) {
+        if (event_flags & USB_HOST_LIB_EVENT_FLAGS_ALL_FREE)
+        {
             ESP_LOGI(TAG, "USB: All devices freed");
             // Continue handling USB events to allow device reconnection
         }
     }
 }
 
+void fill_checkers_cambuff(void)
+{
+    //                  RRRRRGGGGGGBBBBB
+    uint16_t color1 = 0b0010000100000100;
+    uint16_t color2 = 0b1010010100010100;
+    for (int y = 0; y < FRAME_V_RES; ++y)
+    {
+        if (y % 5 == 0)
+        {
+            uint16_t t = color1;
+            color1 = color2;
+            color2 = t;
+        }
+        for (int x = 0; x < FRAME_H_RES; x += 10)
+        {
+            for (int z = 0; z < 5; ++z)
+            {
+                cam_buffer[x + y * FRAME_H_RES + z] = color1;
+                cam_buffer[x + y * FRAME_H_RES + z + 5] = color2;
+            }
+        }
+    }
+}
+
 void init_camera(void)
 {
-
     //@todo does not work in PSRAM... :'(
     cam_buffer = heap_caps_aligned_alloc(64, FRAME_H_RES * FRAME_V_RES * 2, MALLOC_CAP_INTERNAL);
-    if (cam_buffer == NULL) {
+    if (cam_buffer == NULL)
+    {
         ESP_LOGW(TAG, "Insufficient memory for LCD frame buffer. LCD output disabled.");
     }
 
@@ -173,62 +219,66 @@ void init_camera(void)
     // Install USB Host driver. Should only be called once in entire application
     ESP_LOGI(TAG, "Installing USB Host");
     const usb_host_config_t host_config = {
-            .skip_phy_setup = false,
-            .intr_flags = ESP_INTR_FLAG_LEVEL1,
+        .skip_phy_setup = false,
+        .intr_flags = ESP_INTR_FLAG_LEVEL1,
     };
     ESP_ERROR_CHECK(usb_host_install(&host_config));
 
     // Create a task that will handle USB library events
-    if (xTaskCreate(usb_lib_task, "usb_lib", 4096, NULL, 10, NULL) != pdTRUE) {
+    if (xTaskCreate(usb_lib_task, "usb_lib", 4096, NULL, 10, NULL) != pdTRUE)
+    {
         ESP_LOGE(TAG, "Creating USB host lib task failed");
         abort();
     }
 
     // UVC driver install
     const uvc_host_driver_config_t uvc_driver_config = {
-            .driver_task_stack_size = 6 * 1024,
-            .driver_task_priority = 6,
-            .xCoreID = tskNO_AFFINITY,
-            .create_background_task = true,
+        .driver_task_stack_size = 6 * 1024,
+        .driver_task_priority = 6,
+        .xCoreID = tskNO_AFFINITY,
+        .create_background_task = true,
     };
     ESP_ERROR_CHECK(uvc_host_install(&uvc_driver_config));
 
     // UVC Stream init
     const uvc_host_stream_config_t uvc_stream_config = {
-            .event_cb = stream_callback,
-            .frame_cb = frame_callback,
-            .user_ctx = NULL,
-            .usb = {
-                    .vid = 0,
-                    .pid = 0,
-                    .uvc_stream_index = 0,
-            },
-            .vs_format = {
-                    .h_res = FRAME_H_RES,
-                    .v_res = FRAME_V_RES,
-                    .fps = FRAME_FPS,
-                    .format = FRAME_FORMAT,
-            },
-            .advanced = {
-                    .number_of_frame_buffers = NUMBER_OF_FRAME_BUFFERS,
-                    .frame_size = 2 * FRAME_H_RES * FRAME_V_RES,
+        .event_cb = stream_callback,
+        .frame_cb = frame_callback,
+        .user_ctx = NULL,
+        .usb = {
+            .vid = 0,
+            .pid = 0,
+            .uvc_stream_index = 0,
+        },
+        .vs_format = {
+            .h_res = FRAME_H_RES,
+            .v_res = FRAME_V_RES,
+            .fps = FRAME_FPS,
+            .format = FRAME_FORMAT,
+        },
+        .advanced = {
+            .number_of_frame_buffers = NUMBER_OF_FRAME_BUFFERS,
+            .frame_size = 2 * FRAME_H_RES * FRAME_V_RES,
 #if CONFIG_SPIRAM
-                    .frame_heap_caps = MALLOC_CAP_SPIRAM,
+            .frame_heap_caps = MALLOC_CAP_SPIRAM,
 #else
                     .frame_heap_caps = 0,
 #endif
-                    .number_of_urbs = 3,
-                    .urb_size = 4 * 1024,
-            },
+            .number_of_urbs = 3,
+            .urb_size = 4 * 1024,
+        },
     };
 
     BaseType_t ret = xTaskCreate(processing_task, "frame_process", 4 * 1024, NULL, 2, NULL);
     assert(ret == pdPASS);
 
-    while (true) {
+    while (true)
+    {
         ESP_LOGI(TAG, "Opening the stream...");
+        fill_checkers_cambuff();
         esp_err_t err = uvc_host_stream_open(&uvc_stream_config, pdMS_TO_TICKS(5000), &stream);
-        if (ESP_OK != err) {
+        if (ESP_OK != err)
+        {
             ESP_LOGI(TAG, "Failed to open device");
             continue;
         }
--- main/esp32-uvc-opencv-example.c
+++ main/esp32-uvc-opencv-example.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
 #include <pthread.h>
 #include "SDL3/SDL.h"
-#include "SDL3/SDL_esp-idf.h"
-//#include "SDL3_image/SDL_image.h"
 #include "SDL3_ttf/SDL_ttf.h"
 #include "bsp/esp-bsp.h"
 #include "freertos/FreeRTOS.h"
@@ -15,32 +13,39 @@
 
 void print_psram_info();
 
-Uint32 SDLCALL TimerCallback(void *param, SDL_TimerID timerID, Uint32 interval) {
-    (void) param;
-    (void) timerID;
+// ReSharper disable once CppParameterMayBeConstPtrOrRef
+Uint32 SDLCALL TimerCallback(void* param, SDL_TimerID timerID, Uint32 interval)
+{
+    (void)param;
+    (void)timerID;
     // printf("Timer callback executed!\n");
     return interval; // Return the interval to keep the timer running
 }
 
-void *sdl_thread(void *args) {
-    (void) args;
+// ReSharper disable once CppDFAConstantFunctionResult, CppParameterMayBeConstPtrOrRef
+void* sdl_thread(void* args)
+{
+    (void)args;
     printf("SDL3 on ESP32\n");
     print_psram_info();
     print_psram_info();
-    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS) == false) {
+    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS) == false)
+    {
         printf("Unable to initialize SDL: %s\n", SDL_GetError());
         return NULL;
     }
     printf("SDL initialized successfully\n");
 
-    SDL_Window *window = SDL_CreateWindow("SDL on ESP32", BSP_LCD_H_RES, BSP_LCD_V_RES, 0);
-    if (!window) {
+    SDL_Window* window = SDL_CreateWindow("SDL on ESP32", BSP_LCD_H_RES, BSP_LCD_V_RES, 0);
+    if (!window)
+    {
         printf("Failed to create window: %s\n", SDL_GetError());
         return NULL;
     }
 
-    SDL_Renderer *renderer = SDL_CreateRenderer(window, NULL);
-    if (!renderer) {
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, NULL);
+    if (!renderer)
+    {
         printf("Failed to create renderer: %s\n", SDL_GetError());
         SDL_DestroyWindow(window);
         return NULL;
@@ -51,26 +56,29 @@ void *sdl_thread(void *args) {
     SDL_InitFS();
     TestFileOpen("/assets/espressif.bmp");
 
-    TTF_Font *font = initialize_font("/assets/FreeSans.ttf", 12);
+    TTF_Font* font = initialize_font("/assets/FreeSans.ttf", 12);
     if (!font) return NULL;
 
     SDL_TimerID timer_id = SDL_AddTimer(1000, TimerCallback, NULL);
-    if (timer_id == 0) {
+    if (timer_id == 0)
+    {
         printf("Failed to create timer: %s\n", SDL_GetError());
-    } else {
+    }
+    else
+    {
         printf("Timer created successfully\n");
     }
 
-    SDL_Texture *textTexture = render_text(renderer, font, "Hello ESP32 - SDL3", (SDL_Color) {255, 255, 255, 255});
-    SDL_Texture *imageTexture = LoadBackgroundImage(renderer, "/assets/espressif.bmp");
-    SDL_Texture *cameraTexture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGB565, SDL_TEXTUREACCESS_STREAMING,
+    SDL_Texture* textTexture = render_text(renderer, font, "Hello ESP32 - SDL3", (SDL_Color){255, 255, 255, 255});
+    SDL_Texture* imageTexture = LoadBackgroundImage(renderer, "/assets/espressif.bmp");
+    SDL_Texture* cameraTexture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGB565, SDL_TEXTUREACCESS_STREAMING,
                                                    FRAME_H_RES, FRAME_V_RES);
-    SDL_Texture *outputTextureA = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGB565, SDL_TEXTUREACCESS_STREAMING,
-                                                   FRAME_H_RES, FRAME_V_RES);
-    SDL_Texture *outputTextureB = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGB565, SDL_TEXTUREACCESS_STREAMING,
-                                                   FRAME_H_RES, FRAME_V_RES);
-    SDL_Texture *outputTextureC = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGB565, SDL_TEXTUREACCESS_STREAMING,
-                                                   FRAME_H_RES, FRAME_V_RES);
+    SDL_Texture* outputTextureA = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGB565, SDL_TEXTUREACCESS_STREAMING,
+                                                    FRAME_H_RES, FRAME_V_RES);
+    SDL_Texture* outputTextureB = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGB565, SDL_TEXTUREACCESS_STREAMING,
+                                                    FRAME_H_RES, FRAME_V_RES);
+    SDL_Texture* outputTextureC = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGB565, SDL_TEXTUREACCESS_STREAMING,
+                                                    FRAME_H_RES, FRAME_V_RES);
 
     float bmp_x = 2.0f, bmp_y = 2.0f;
     float bmp_speed_x = 2.0f, bmp_speed_y = 2.0f;
@@ -84,15 +92,20 @@ void *sdl_thread(void *args) {
     print_psram_info();
     init_image_processing();
     printf("Entering main loop...\n");
-//    heap_trace_dump();
+    //    heap_trace_dump();
     SDL_Event event;
-    while (1) {
-        while (SDL_PollEvent(&event)) {
-            if (event.type == SDL_EVENT_QUIT) {
+    while (1)
+    {
+        while (SDL_PollEvent(&event))
+        {
+            if (event.type == SDL_EVENT_QUIT)
+            {
                 break;
-            } else if (event.type == SDL_EVENT_FINGER_UP) {
-                bmp_x = (float) event.tfinger.x;
-                bmp_y = (float) event.tfinger.y;
+            }
+            else if (event.type == SDL_EVENT_FINGER_UP)
+            {
+                bmp_x = (float)event.tfinger.x;
+                bmp_y = (float)event.tfinger.y;
                 printf("Finger up [%f, %f]\n", bmp_x, bmp_y);
             }
         }
@@ -124,10 +137,11 @@ void *sdl_thread(void *args) {
         SDL_UnlockTexture(cameraTexture);
 
         draw_image(renderer, cameraTexture, 50, 50, FRAME_H_RES, FRAME_V_RES);
-        if(is_image_processing_done()) {
-            void *pixelsA;
-            void *pixelsB;
-            void *pixelsC;
+        if (is_image_processing_done())
+        {
+            void* pixelsA;
+            void* pixelsB;
+            void* pixelsC;
             SDL_LockTexture(outputTextureA, &standardRect, &pixelsA, &pitch);
             assert(pitch == FRAME_H_RES * 2);
             SDL_LockTexture(outputTextureB, &standardRect, &pixelsB, &pitch);
@@ -153,32 +167,34 @@ void *sdl_thread(void *args) {
 #define HEAP_TRACE_MAX_STACK (450)
 
 //static heap_trace_record_t record_buffer[HEAP_TRACE_MAX_STACK];
-void app_main(void) {
-    (void) app_main;
+void app_main(void)
+{
+    (void)app_main;
     pthread_t sdl_pthread;
 
     pthread_attr_t attr;
     print_psram_info();
-//    heap_trace_init_standalone(record_buffer, HEAP_TRACE_MAX_STACK);
-//    heap_trace_start(HEAP_TRACE_ALL);
+    //    heap_trace_init_standalone(record_buffer, HEAP_TRACE_MAX_STACK);
+    //    heap_trace_start(HEAP_TRACE_ALL);
     pthread_attr_init(&attr);
-    pthread_attr_setstacksize(&attr, 28000);  // Set the stack size for the thread
+    pthread_attr_setstacksize(&attr, 28000); // Set the stack size for the thread
 
     int ret = pthread_create(&sdl_pthread, &attr, sdl_thread, NULL);
-    if (ret != 0) {
+    if (ret != 0)
+    {
         printf("Failed to create SDL thread: %d\n", ret);
         return;
     }
 
     pthread_detach(sdl_pthread);
     init_camera();
-
 }
 
-void print_psram_info() {
+void print_psram_info()
+{
     printf("PSRAM HEAP: total %d, free: %d; largest free block: %d\n\r",
            heap_caps_get_total_size(MALLOC_CAP_SPIRAM),
            heap_caps_get_free_size(MALLOC_CAP_SPIRAM),
            heap_caps_get_largest_free_block(MALLOC_CAP_SPIRAM)
-           );
+    );
 }
